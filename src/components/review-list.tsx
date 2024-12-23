5d7c14fff035c34b6cc1c914d3d692f8275d2d61
Gleb Kochergin <kochergin.gleb2016@yandex.ru>
2024-12-23 11:09:58 +0500

    made some fixes

--- src/components/review-list.tsx
+++ src/components/review-list.tsx
@@ -6,9 +6,15 @@ type ReviewListProps = {
 }
 
 export function ReviewList({reviews: reviewList}: ReviewListProps) {
+  const sortedReviewList = reviewList
+    .slice()
+    .sort(
+      (a, b) =>
+        new Date(b.date).getTime() - new Date(a.date).getTime()
+    ).slice(0, 10);
   return (
     <ul className="reviews__list">
-      {reviewList.map((review) => (
+      {sortedReviewList.map((review) => (
         <Review key={review.id} review={review}/>
       ))}
     </ul>
--- src/pages/favorites-page.tsx
+++ src/pages/favorites-page.tsx
@@ -7,12 +7,37 @@ import {Footer} from '../components/footer.tsx';
 export function FavoritesPage(): React.JSX.Element {
   const offers = useAppSelector((state) => state.offers);
   const favoriteOffers = offers.filter((offer) => offer.isFavorite);
+  if (!favoriteOffers.length) {
+    return (
+      <div className="page">
+        <Header/>
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">
+                Favorites (empty)
+              </h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">
+                  Nothing yet saved.
+                </b>
+                <p className="favorites__status-description">
+                  Save properties to narrow down search or plan your future trips.
+                </p>
+              </div>
+            </section>
+          </div>
+        </main>
+        <Footer/>
+      </div>
+    );
+  }
   return (
     <div className="page">
       <Header/>
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
-          <FavoritesList favorites={favoriteOffers} />
+          <FavoritesList favorites={favoriteOffers}/>
         </div>
       </main>
       <Footer/>
